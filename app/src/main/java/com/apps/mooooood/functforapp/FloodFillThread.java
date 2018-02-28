4bc418499b4060f19bf21b73e32bf3256f3ea7cc
mooooood <dfulle17@gmail.com>
2018-02-27 21:19:36 -0500

    First Commit 02272018

---
+++ app/src/main/java/com/apps/mooooood/functforapp/FloodFillThread.java
@@ -0,0 +1,70 @@
+package com.apps.mooooood.functforapp;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by mooooood on 2/20/18.
+ */
+
+public class FloodFillThread extends Thread {
+
+    ProgressDialog mProgressDialog;
+    Bitmap mBitmap;
+    int               mTargetColor;
+    int            mNewColor;
+    Point mPoint;
+    Runnable       mCallback;
+
+
+    //QueueLinearFloodFiller q = new QueueLinearFloodFiller(test);
+    public FloodFillThread(ProgressDialog pd, Runnable callback,
+                           Bitmap bitmap, Point pt, int targetColor, int newColor)
+    {
+        mBitmap          = bitmap;
+        mPoint           = pt;
+        mTargetColor     = targetColor;
+        mNewColor          = newColor;
+        mProgressDialog = pd;
+        mCallback       = callback;
+    }
+    //QueueLinearFloodFiller q = new QueueLinearFloodFiller(test);
+    public FloodFillThread( Runnable callback,
+                           Bitmap bitmap, Point pt, int targetColor, int newColor)
+    {
+        mBitmap          = bitmap;
+        mPoint           = pt;
+        mTargetColor     = targetColor;
+        mNewColor          = newColor;
+        mCallback       = callback;
+    }
+
+    @Override
+    public void run()
+    {
+        QueueLinearFloodFiller filler =
+                new QueueLinearFloodFiller(mBitmap, mTargetColor, mNewColor);
+
+        filler.setTolerance(10);
+
+        filler.floodFill(mPoint.x, mPoint.y);
+
+        handler.sendEmptyMessage(0);
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            mProgressDialog.dismiss();
+            mCallback.run();
+        }
+    };
+
+}
---
+++ app/src/main/java/com/apps/mooooood/functforapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.apps.mooooood.functforapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val MAINTAG = "MAIN-DBUG"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Open CV Button - not in paint functions
+        // Image load, different filter functions
+        opcv_btn.setOnClickListener({
+            val opCVIntent = Intent(this, OpCVImage_using::class.java)
+            startActivity(opCVIntent)
+        })
+
+        // Paint Activity Button
+        // Image load, filter image, user paint activity - actual paint activity
+        paint_btn.setOnClickListener({
+            val paintIntent = Intent(this, UserPaint_using::class.java)
+            startActivity(paintIntent)
+        })
+
+        close_btn.setOnClickListener({
+            Log.d(MAINTAG,"close app onclick")
+            finish()
+            //   System.exit(0)
+
+        })
+
+    }
+}
